def155651a6c9a4c03d2d0456108bb728d117002
Joabesv <joabevarjao123@gmail.com>
2023-12-24 19:18:16 -0300

    feat: convertUfabcDiscplinasr

---
+++ packages/common/lib/convertUfabcDiscplinas.ts
@@ -0,0 +1,175 @@
+import {
+  camelCase,
+  chunk as lodashChunk,
+  get as lodashGet,
+  startCase,
+} from 'lodash-es';
+import { logger } from './logger';
+
+type Disciplina = {
+  id: string;
+  campus?: 'santo andre' | 'sao bernardo' | null;
+  turno?: 'noturno' | 'diurno' | 'tarde' | null;
+  horarios: string | string[];
+  nome: string;
+  turma?: string;
+  disciplina: string;
+  disciplina_id: string;
+  teoria: string | null;
+  pratica: string | null;
+};
+
+// This convert an disciplina from the .json from matriculas.ufabc
+export function convertUfabcDisciplinas(disciplina: Disciplina) {
+  const clonedDisciplinas = structuredClone(disciplina);
+  clonedDisciplinas.campus = undefined;
+  clonedDisciplinas.turno = undefined;
+
+  let afterNoon = false;
+
+  const isNoon =
+    clonedDisciplinas.horarios &&
+    typeof clonedDisciplinas.horarios === 'object';
+  // handle horarios based on pdf or json
+  if (isNoon) {
+    const startHours: string[] = lodashGet(
+      clonedDisciplinas.horarios,
+      '[0].horas',
+      [],
+    );
+
+    afterNoon = ['14:00', '15:00', '16:00', '17:00'].some((hour) =>
+      startHours.includes(hour),
+    );
+  } else if (
+    clonedDisciplinas.horarios &&
+    typeof clonedDisciplinas.horarios === 'string'
+  ) {
+    clonedDisciplinas.horarios = removeLineBreaks(clonedDisciplinas.horarios);
+
+    const matchedHorarios = clonedDisciplinas.horarios.match(/\d{2}:\d{2}/g);
+
+    if (matchedHorarios?.length % 2 === 0) {
+      const hours = lodashChunk(matchedHorarios, 2);
+      hours.forEach((hour) => {
+        const [start] = hour.map((h) => Number.parseInt(h.split(':')[0] ?? ''));
+        if (start >= 12 && start < 18) {
+          afterNoon = true;
+        }
+      });
+    }
+  }
+
+  // trabalha nas disciplinas
+  if (!clonedDisciplinas.nome) {
+    return clonedDisciplinas;
+  }
+
+  let turnoIndex: number | null = null;
+  let breakRule = '-';
+  let splitted = removeLineBreaks(clonedDisciplinas.nome).split(breakRule);
+  if (splitted.length === 1) {
+    breakRule = ' ';
+    splitted = splitted[0]!.split(/\s+/);
+  }
+  logger.info({ msg: 'Splitted teste', splitted });
+  splitted.forEach((item, i) => {
+    // Theres probably a bug in here
+    clonedDisciplinas.campus =
+      clonedDisciplinas.campus || extractCampus(item as Disciplina['campus']);
+    clonedDisciplinas.turno =
+      clonedDisciplinas.turno || extractTurno(item as Disciplina['turno']);
+
+    if (
+      (clonedDisciplinas.turno || clonedDisciplinas.campus) &&
+      turnoIndex === null
+    ) {
+      turnoIndex = i;
+    }
+  });
+
+  if (afterNoon && !clonedDisciplinas.turno) {
+    // Theres probably a bug here too
+    clonedDisciplinas.turno = 'tarde';
+  }
+
+  if (!clonedDisciplinas.campus) {
+    if (!turnoIndex) {
+      return null;
+    }
+    const secondPath = splitted.slice(turnoIndex + 1, splitted.length);
+    clonedDisciplinas.campus = extractCampus(secondPath.join(breakRule));
+  }
+
+  // cut until the index we found
+  splitted = splitted.slice(0, turnoIndex!);
+
+  // separa a turma da disciplina
+  const ufabcDisciplina = splitted.join('-').split(/\s+/).filter(Boolean);
+  clonedDisciplinas.turma = ufabcDisciplina.at(-1);
+  ufabcDisciplina.pop();
+  logger.info(ufabcDisciplina);
+  // fix disciplina
+  clonedDisciplinas.disciplina = ufabcDisciplina.join(' ').trim();
+
+  clonedDisciplinas.disciplina_id = clonedDisciplinas.id;
+
+  if (clonedDisciplinas.teoria === '0' || clonedDisciplinas.teoria === '') {
+    clonedDisciplinas.teoria = null;
+  }
+  if (clonedDisciplinas.pratica === '0' || clonedDisciplinas.pratica === '') {
+    clonedDisciplinas.pratica = null;
+  }
+
+  if (clonedDisciplinas.teoria !== null) {
+    clonedDisciplinas.teoria = cleanTeacher(
+      removeLineBreaks(clonedDisciplinas.teoria),
+    );
+  }
+  if (clonedDisciplinas.pratica !== null) {
+    clonedDisciplinas.pratica = cleanTeacher(
+      removeLineBreaks(clonedDisciplinas.pratica),
+    );
+  }
+
+  return clonedDisciplinas;
+}
+
+const removeLineBreaks = (str: string) => str.replaceAll(/\r?\n|\r/g, ' ');
+
+const extractCampus = (disciplina: Disciplina['campus']) => {
+  const min = disciplina?.toLowerCase();
+  if (!min) {
+    return null;
+  }
+  if (/.*santo\s+andre.*/.test(min)) {
+    return 'santo andre';
+  }
+
+  if (/.*sao\s+bernardo.*/.test(min)) {
+    return 'sao bernardo';
+  }
+
+  return null;
+};
+
+const extractTurno = (disciplina: Disciplina['turno']) => {
+  // TODO: include `vespertino`
+
+  const min = disciplina?.toLowerCase();
+  if (min?.includes('diurno') || min?.includes('matutino')) {
+    return 'diurno';
+  }
+
+  if (min?.includes('noturno')) {
+    return 'noturno';
+  }
+
+  return null;
+};
+
+const cleanTeacher = (str: string) => {
+  return startCase(camelCase(str))
+    .replaceAll(/-+.*?-+/g, '')
+    .replaceAll(/\(+.*?\)+/g, '');
+};
