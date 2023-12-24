1a76e83dc08678be069cd52792521bd7e3c5fc8f
Joabesv <joabevarjao123@gmail.com>
2023-12-23 21:46:59 -0300

    test: remove test

--- apps/queue/src/setup.ts
+++ apps/queue/src/setup.ts
@@ -7,7 +7,7 @@ import { updateUserEnrollmentsWorker } from './jobs/enrollments/updateUserEnroll
 import { syncWorker } from './jobs/matriculas/sync.js';
 import { updateTeachersWorker } from './jobs/enrollments/updateTeachers.js';
 
-logger.info('Starting queue workers');
+logger.info('[QUEUE] Starting workers');
 
 const emailWorker = createWorker('Send:Email', sendEmailWorker);
 const enrollmentsWorker = createWorker(
@@ -28,18 +28,18 @@ const updateTeachersEnrollmentsWorker = createWorker(
   updateTeachersWorker,
 );
 
-logger.info('Queue workers started');
+logger.info('[QUEUE] Workers started');
 
 emailWorker.on('completed', (job) => {
   logger.info(`Job ${job.queueName} completed`);
 });
 
 enrollmentsWorker.on('completed', (job) => {
-  logger.info(`Job ${job.id} completed`);
+  logger.info(`Job ${job.queueName} completed`);
 });
 
 userEnrollmentsWorker.on('completed', (job) => {
-  logger.info(`Job ${job.id} completed`);
+  logger.info(`Job ${job.queueName} completed`);
 });
 
 syncMatriculasWorker.on('completed', (job) => {
@@ -54,7 +54,7 @@ gracefullyShutdown({ delay: 500 }, async ({ err, signal }) => {
   if (err) {
     logger.fatal({ err }, 'error starting app');
   }
-  logger.info({ signal }, 'Gracefully shutting down workers');
+  logger.info({ signal }, '[QUEUE] Gracefully shutting down workers');
 
   await Promise.all([
     emailWorker.close(),
