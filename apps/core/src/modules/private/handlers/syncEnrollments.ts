1af8573b5517713b5b8977e0497765a04903198c
Joabesv <joabevarjao123@gmail.com>
2023-12-27 12:52:51 -0300

    wip: sync(enrollments)

--- apps/core/src/modules/private/handlers/syncEnrollments.ts
+++ apps/core/src/modules/private/handlers/syncEnrollments.ts
@@ -1,11 +1,16 @@
 import { createHash } from 'node:crypto';
 import { DisciplinaModel } from '@next/models';
 import { ofetch } from 'ofetch';
+import { convertUfabcDisciplinas } from '@next/common';
 import { type ParseXlSXBody, parseXlsx } from '../utils/parseXlsx.js';
 import type { FastifyReply, FastifyRequest } from 'fastify';
 
 export type SyncEnrollmentsRequest = {
-  Body: ParseXlSXBody;
+  Body: ParseXlSXBody & {
+    hash: string;
+    year: number;
+    quad: number;
+  };
 };
 
 export async function syncEnrollments(
@@ -48,24 +53,30 @@ export async function syncEnrollments(
     )
     .lean({ virtuals: true });
 
+  // eslint-disable-next-line unused-imports/no-unused-vars
   const disciplinasMap = disciplinas.map((d): any => [d._id, d]);
-  const keys = ['ra', 'year', 'quad', 'disciplina'];
-  const unlinkedEnrollments = await parseXlsx(request.body);
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const _keys = ['ra', 'year', 'quad', 'disciplina'];
+  const rawEnrollments = (await parseXlsx(request.body)).map(
+    (ufabcDisciplina) => convertUfabcDisciplinas(ufabcDisciplina),
+  );
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  const _filteredEnrollments = rawEnrollments
+    .filter((enrollment) => enrollment?.ra)
+    .map((studentEnrollment) =>
+      Object.assign({}, studentEnrollment, { year, quad }),
+    );
 
-  // const enrollments = (await app.helpers.parse.pdf(request.body))
-  //   .map(app.helpers.transform.disciplinas)
-  //   .filter((enrollment) => enrollment.ra && enrollment.disciplina)
-  //   .map((e) => _.extend(e, { year, quad }))
-  //   .map((e) =>
-  //     _.extend(e, {
-  //       ..._.omit(
-  //         disciplinasMap.get(app.helpers.transform.identifier(e)) || {},
-  //         ['id', '_id'],
-  //       ),
-  //       identifier: app.helpers.transform.identifier(e, keys),
-  //       disciplina_identifier: app.helpers.transform.identifier(e),
-  //     }),
-  //   );
+  // const enrollments = filteredEnrollments.map((enrollment) =>
+  //   _.extend(enrollment, {
+  //     ..._.omit(disciplinasMap.get(app.helpers.transform.identifier(e)) || {}, [
+  //       'id',
+  //       '_id',
+  //     ]),
+  //     identifier: app.helpers.transform.identifier(e, keys),
+  //     disciplina_identifier: app.helpers.transform.identifier(e),
+  //   }),
+  // );
 
   const enrollmentsHash = createHash('md5')
     .update(JSON.stringify({ name: 'Joabe' }))
@@ -84,5 +95,5 @@ export async function syncEnrollments(
   // app.agenda.schedule('in 2 minutes', 'updateEnrollments', { json: chunks[1] });
   // app.agenda.schedule('in 4 minutes', 'updateEnrollments', { json: chunks[2] });
 
-  return { published: true, enrollments };
+  return reply.send({ published: true });
 }
