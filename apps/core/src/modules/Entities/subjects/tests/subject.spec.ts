04aa831fb9c2496cd86c657870ee01a578d57602
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-07-01 09:43:10 -0300

    chore(tests): add more subject tests

--- apps/core/src/modules/Entities/subjects/tests/subject.spec.ts
+++ apps/core/src/modules/Entities/subjects/tests/subject.spec.ts
@@ -4,9 +4,13 @@ import mongoose from 'mongoose';
 import { GenericContainer, Wait } from 'testcontainers';
 import { SubjectModel } from '@/models/Subject.js';
 import { SubjectRepository } from '../subjects.repository.js';
+import { SubjectService } from '../subjects.service.js';
+import { camelCase, startCase } from 'lodash-es';
 
-describe('Subjects repository unit tests', () => {
+describe('Subjects service unit tests', () => {
   let connection: typeof mongoose;
+  const subjectRepository = new SubjectRepository(SubjectModel);
+  const subjectService = new SubjectService(subjectRepository);
 
   before(async () => {
     const mongoContainer = await new GenericContainer('mongo:4.0.1')
@@ -25,18 +29,51 @@ describe('Subjects repository unit tests', () => {
   });
 
   it('Should create a subject', async () => {
-    const subjectRepository = new SubjectRepository(SubjectModel);
-
-    const createdSubject = await subjectRepository.createSubject({
-      name: 'Algebra Linear',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
+    const createdSubject = await subjectService.createSubject('Algebra Linear');
 
     const subjectInDb = await subjectRepository.listSubject({
       name: 'Algebra Linear',
     });
 
     assert.strictEqual(createdSubject.name, subjectInDb[0].name);
+  });
+
+  it('Should search for a subject', async () => {
+    const subjects = [
+      {
+        name: 'Matemática Discreta',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Cálculo I',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Cálculo II',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    await Promise.all(
+      subjects.map(async (subject) => {
+        subjectService.createSubject(subject.name);
+      }),
+    );
+
+    const normalizedSearch = startCase(camelCase('cálculo'));
+    const validatedSearch = normalizedSearch.replaceAll(
+      /[\s#$()*+,.?[\\\]^{|}-]/g,
+      '\\$&',
+    );
+    const search = new RegExp(validatedSearch, 'gi');
+    const searchResults = await subjectService.findSubject(search);
+
+    assert.strictEqual(
+      searchResults.data.filter((s) => s.name === 'Cálculo I').length,
+      1,
+    );
   });
 });
